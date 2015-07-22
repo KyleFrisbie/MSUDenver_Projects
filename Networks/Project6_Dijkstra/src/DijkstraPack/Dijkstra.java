0ba024b74e51f3699b2a42c7f99403b03948a9e8
kyle.l.frisbie@gmail.com <kyle.l.frisbie@gmail.com>
2015-07-21 21:55:47 -0600

    project near completion, needs more comments.

--- Networks/Project6_Dijkstra/src/DijkstraPack/Dijkstra.java
+++ Networks/Project6_Dijkstra/src/DijkstraPack/Dijkstra.java
@@ -2,7 +2,9 @@ package DijkstraPack;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Kyle on 7/21/2015.
@@ -13,19 +15,24 @@ public class Dijkstra {
     private ArrayList<Node> nodeCollection;
     private int nRouters;
     private Scanner inputFile;
-
+    private String[] edgeTracker;
 
     public class Node implements Comparable<Node> {
         public final String name;
         public final int id;
-        public ArrayList<Edge> edges = new ArrayList<>();
-        public double minDistance = Double.POSITIVE_INFINITY;
-        public Node previous;
+        public ArrayList<Edge> edges = new ArrayList<>();   // Y'
+        public double minDistance = Double.POSITIVE_INFINITY;   // D(i)
+        public Node previous;   // p(i)
 
         @Override
         public int compareTo(Node o) {
             return Double.compare(minDistance, o.minDistance);
         }
+
+        public int compareTo(Node o1, Node o2) {
+            return Double.compare(o1.minDistance, o2.minDistance);
+        }
+
 
         public Node(String nodeName, int nodeID) {
             name = nodeName;
@@ -43,6 +50,11 @@ public class Dijkstra {
         }
     }
 
+    /**
+     * Display a prompt message to ask the user to input the total number of
+     * routers, n, in the network. Validate n to make sure that it is greater
+     * than or equal to 2.
+     */
     private void promptForNodeN() {
         do {
             System.out.print("Input the total number " +
@@ -50,18 +62,27 @@ public class Dijkstra {
             nRouters = Integer.parseInt(kb.nextLine());
         } while (nRouters < 2);
         nodeCollection = new ArrayList<>(nRouters * 2);
+        edgeTracker = new String[nRouters + 1];
     }
 
+
+    /**
+     * Validate values in input file such that the first and the second numbers
+     * in each row are between 1 and n, the third number needs to be validated
+     * to be positive. Record the cost information in the Cost matrix.
+     *
+     * @return true if file is valid
+     */
     private boolean scanInputFile() {
         String lineIn;
         String[] tokens;
-        double[] numTokens;
+        int[] numTokens;
         int lineNumber = 1;
         while (inputFile.hasNext()) {
             lineIn = inputFile.nextLine();
             tokens = lineIn.split("\\s+");
 
-            numTokens = new double[]{
+            numTokens = new int[]{
                     Integer.parseInt(tokens[0]),
                     Integer.parseInt(tokens[1]),
                     Integer.parseInt(tokens[2])
@@ -103,7 +124,6 @@ public class Dijkstra {
             primary.edges.add(new Edge(secondary,
                     Double.parseDouble(tokens[2])));
 
-            //todo: need to add this edge to secondary node edge[]?
             secondary.edges.add(new Edge(primary,
                     Double.parseDouble(tokens[2])));
 
@@ -112,6 +132,17 @@ public class Dijkstra {
         return true;
     }
 
+    /**
+     * Display a prompt message to ask the user to input the name of a txt file
+     * that contains costs of all links. Each line in this file provides the
+     * cost between a pair of routers as below, where tab (\t) is used to
+     * separate the numbers in each line. This txt file can locate in the
+     * same directory where this program runs such that a path is not needed.
+     * Display a message saying that in which row the first
+     * invalid number is detected, close the txt file, and KEEP asking for
+     * the name of the cost input file until all numbers are checked to be
+     * valid.
+     */
     private void promptForInputFile() {
         boolean validFile;
         do {
@@ -128,40 +159,138 @@ public class Dijkstra {
         } while (!validFile);
     }
 
+    private void arrayPrintOut(ArrayList<Node> nPrime, ArrayList<String> yPrime) {
+        System.out.println("__________________________________________");
+        System.out.print("N': [ ");
+        for (Node n : nPrime) {
+            if (nPrime.indexOf(n) != nPrime.size() - 1) {
+                System.out.print(n.name + ", ");
+            } else {
+                System.out.println(n.name + " ]");
+            }
+        }
+
+        if (yPrime.size() == 0) {
+            System.out.println("Y': null");
+        } else {
+            System.out.print("Y': [ ");
+            for (String y : yPrime) {
+                if (yPrime.indexOf(y) != yPrime.size() - 1) {
+                    System.out.print(y + ", ");
+                } else {
+                    System.out.println(y + " ]");
+                }
+            }
+        }
+        System.out.println("------------------------------------------");
+
+        System.out.print("Node:" + "\t" + "D(i)[]:" + "\t" + "p(i)[]: ");
+        System.out.println();
+        for (Node e : nodeCollection) {
+            try {
+                System.out.print(e.name + "\t\t" + e.minDistance + "\t\t" + e.previous.name);
+            } catch (Exception expt) {
+                System.out.print(e.name + "\t\t" + "inf" + "\t\t" + "-");
+            }
+            System.out.println();
+        }
+
+        System.out.println("__________________________________________");
+    }
+
+    /**
+     * Implement the Dijkstra's algorithm to build up the shortest-path tree
+     * rooted at source router V1. As the intermediate results, at the end of
+     * Initialization and each iteration of the Loop, display
+     * <p>
+     * The set N': this is stored in the Node.name value for each Node in
+     * nodeCollection.
+     * The set Y' : this is stored in Node.edges which is an array list of each
+     * edge associated with that node.
+     * D(i) for each i between 2 and n: stored as Node.minDistance
+     * p(i) for each i between 2 and n: Node.previous value for each Node in
+     * nodeCollection.
+     */
     private void walkPaths() {
         Node n = nodeCollection.get(0);
+        n.previous = n;
         n.minDistance = 0;
 
-        PriorityQueue<Node> nodeQueue = new PriorityQueue<>();
-        nodeQueue.add(n);
+        ArrayList<Node> nPrime = new ArrayList<>();
+        ArrayList<String> yPrime = new ArrayList<>();
 
-        while (!nodeQueue.isEmpty()) {
-            Node x = nodeQueue.poll();
+        List<Node> nodeList = new ArrayList<>();
+        nodeList.add(n);
 
-            for (Edge e : x.edges) {
-                Node next = e.node;
-                double length = e.length;
+        while (!nodeList.isEmpty()) {
+            nodeList.stream().sorted(Node::compareTo);
+            Node x = nodeList.get(0);
 
+            nodeList.remove(x);
+
+            if (!nPrime.contains(x)) {
+                nPrime.add(x);
+            }
+
+            arrayPrintOut(nPrime, yPrime);
+
+            while (x.edges.size() > 0) {
+                Edge e = x.edges.get(0);
+                Edge min = e;
+                for (Edge edge : x.edges) {
+                    if (edge.length < e.length) {
+                        min = edge;
+                    }
+                }
+                x.edges.remove(min);
+
+                Node next = min.node;
+                if (!nPrime.contains(min.node)) {
+                    nPrime.add(min.node);
+                }
+                double length = min.length;
+
+                // D(k) + c(k, i)
                 double testLengthDistance = x.minDistance + length;
+                // D(k) + c(k, i) < D(i)
                 if (testLengthDistance < next.minDistance) {
-                    nodeQueue.remove(next);
+                    nodeList.remove(next);
+                    // D(i) = D(k) + c(k, i)
                     next.minDistance = testLengthDistance;
+                    // p(i) = k
                     next.previous = x;
-                    nodeQueue.add(next);
+                    edgeTracker[next.id] = x.name;
+                    if (!yPrime.contains("(" + x.name + ", " + min.node.name + ")")) {
+                        yPrime.add("(" + x.name + ", " + min.node.name + ")");
+                    }
+                    nodeList.add(next);
+                    arrayPrintOut(nPrime, yPrime);
                 }
             }
         }
     }
 
-    private List<Node> getShortestPath(Node n) {
-        List<Node> bestPath = new ArrayList<>();
-        for (Node node = n; node != null; node = node.previous) {
-            bestPath.add(node);
+    /**
+     * Use the shortest-path tree resulted from the Dijkstra's algorithm to
+     * build up the forwarding table for router V1. Display the forwarding
+     * table in the following format:
+     * <p>
+     * Destination Link
+     * V2 (V1, ...)
+     * V3 (V1, ...)
+     * ...
+     * Vn (V1, ...)
+     */
+    private String[] forwardingTable() {
+        String[] links = new String[nRouters + 1];
+        for (Node i : nodeCollection) {
+            Node j = i;
+            while (j.previous != nodeCollection.get(0)) {
+                j = j.previous;
+            }
+            links[i.id] = j.name;
         }
-
-        Collections.reverse(bestPath);
-
-        return bestPath;
+        return links;
     }
 
     public static void main(String[] args) {
@@ -169,34 +298,18 @@ public class Dijkstra {
         driver.promptForNodeN();
         driver.promptForInputFile();
 
-        for (Node n : driver.nodeCollection) {
-            for (Edge e : n.edges) {
-                System.out.println(n.name + "\t" + e.node.name + "\t" + e.length);
-            }
-        }
-
-        System.out.println("___________________________________________");
-
         driver.walkPaths();
 
-        for (Node n : driver.nodeCollection) {
-            for (Edge e : n.edges) {
-                System.out.println(n.name + "\t" + e.node.name + "\t" + e.length);
-            }
-        }
 
-        System.out.println("___________________________________________");
+        System.out.println("Shortest Path Tree:");
+        System.out.println("Destination\tLink");
 
+        String[] linkPath = driver.forwardingTable();
 
-        for (Node n : driver.nodeCollection) {
-
-            List<Node> bestPath = driver.getShortestPath(n);
-            try {
-                System.out.println(n.name + "\t" + n.previous.name + "\t" + n.minDistance);
-            } catch (Exception e) {
-                System.out.println("Exception.");
-            }
-
+        for (int i = 0; i < linkPath.length - 1; i++) {
+            System.out.println(driver.nodeCollection.get(i).name + "\t" +
+                    "(" + driver.nodeCollection.get(0).name +
+                    ", " + linkPath[i + 1] + ")");
         }
     }
 }
