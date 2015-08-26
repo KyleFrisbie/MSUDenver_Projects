2876dd02d285187b1db1a11b6339539c97861aa4
kyle.l.frisbie@gmail.com <kyle.l.frisbie@gmail.com>
2015-08-26 16:31:47 -0600

    color picker

--- MobileProgramming/ColorPicker/app/src/main/java/com/kylefrisbie/colorpicker/app/MainActivity.java
+++ MobileProgramming/ColorPicker/app/src/main/java/com/kylefrisbie/colorpicker/app/MainActivity.java
@@ -1,17 +1,73 @@
 package com.kylefrisbie.colorpicker.app;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
 
 
 public class MainActivity extends Activity {
+
+    private NumberPicker mRedButton, mBlueButton, mGreenButton;
+    private ImageView mColorBox;
+    private final int MIN_VALUE = 0;
+    private final int MAX_VALUE = 255;
+    private int red, green, blue = 0;
+
+    protected void initializeNumberPicker(NumberPicker picker) {
+        picker.setMinValue(MIN_VALUE);
+        picker.setMaxValue(MAX_VALUE);
+    }
+
+    protected void resetBackgroundColor() {
+        mColorBox.setBackgroundColor(Color.rgb(red, green, blue));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mColorBox = (ImageView)findViewById(R.id.imageView);
+        resetBackgroundColor();
+
+        mRedButton = (NumberPicker)findViewById(R.id.numberPicker_Red);
+        initializeNumberPicker(mRedButton);
+
+        mRedButton.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                red = newVal;
+                resetBackgroundColor();
+            }
+        });
+
+        mGreenButton = (NumberPicker)findViewById(R.id.numberPicker_Green);
+        initializeNumberPicker(mGreenButton);
+
+        mGreenButton.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                green = newVal;
+                resetBackgroundColor();
+            }
+        });
+
+        mBlueButton = (NumberPicker)findViewById(R.id.numberPicker_Blue);
+        initializeNumberPicker(mBlueButton);
+
+        mBlueButton.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                blue = newVal;
+                resetBackgroundColor();
+            }
+        });
     }
 
 
@@ -22,18 +78,18 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
